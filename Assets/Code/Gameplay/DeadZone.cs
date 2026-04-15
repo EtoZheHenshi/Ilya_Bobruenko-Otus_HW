c675052fc2b8f44a79e48344b6608722e43c174c
Ilya Bobruenko <henshiwork@gmail.com>
2026-04-15 12:59:09 +0300

    Add winning restart level

--- Assets/Code/Gameplay/DeadZone.cs
+++ Assets/Code/Gameplay/DeadZone.cs
@@ -1,3 +1,4 @@
+using System;
 using Audio;
 using InputLogic;
 using UnityEngine;
@@ -7,11 +8,32 @@ namespace Gameplay
 {
     public sealed class DeadZone : MonoBehaviour
     {
+        [SerializeField] private Transform bricks;
         [SerializeField] public SoundData gameOverSound;
+        [SerializeField] public SoundData winningSound;
+
+        private void Update()
+        {
+            if (bricks.childCount == 0)
+            {
+                Winning();
+            }
+        }
         
         public void GameOver()
         {
             AudioManager.Instance.PlaySound(gameOverSound);
+            RestartLevel();
+        }
+        
+        private void Winning()
+        {
+            AudioManager.Instance.PlaySound(winningSound);
+            RestartLevel();
+        }
+
+        private void RestartLevel()
+        {
             InputManager.Instance.GameplayInput.Clear();
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
