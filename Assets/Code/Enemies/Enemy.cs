44b0ccc4d05835f4d5b6250774bc753dd60dce3e
EtoZheHenshi <henshiwork@gmail.com>
2026-05-08 17:00:10 +0300

    Add NavMeshSurfaces for enemy

--- Assets/Code/Enemies/Enemy.cs
+++ Assets/Code/Enemies/Enemy.cs
@@ -1,13 +1,18 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Code.Enemies
 {
+    [RequireComponent(typeof(EnemyHitFlash))]
     public abstract class Enemy : MonoBehaviour, IDamageable
     {
         [SerializeField] private int maxHealth;
+        [SerializeField] private GameObject player;
         
         private int _currentHealth;
         private EnemyHitFlash _hitFlash;
+        private NavMeshAgent _agent;
         
         public int MaxHealth => maxHealth;
         public int CurrentHealth => _currentHealth;
@@ -16,6 +21,17 @@ namespace Code.Enemies
         {
             _currentHealth = maxHealth;
             _hitFlash = GetComponent<EnemyHitFlash>();
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            MoveToPlayer();
+        }
+
+        private void MoveToPlayer()
+        {
+            _agent.SetDestination(player.transform.position);
         }
 
         public void TakeDamage(int damage)
