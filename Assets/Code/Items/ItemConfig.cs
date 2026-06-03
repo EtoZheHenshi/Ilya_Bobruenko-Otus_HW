99bcd1f505dd4bb724068c1ff55f24731c17a684
EtoZheHenshi <henshiwork@gmail.com>
2026-06-03 07:01:12 +0300

    Add life time for drop items

--- Assets/Code/Items/ItemConfig.cs
+++ Assets/Code/Items/ItemConfig.cs
@@ -7,8 +7,10 @@ namespace Code.Items
     {
         [SerializeField] private Collider _itemPrefab;
         [SerializeField] private ItemEffect _itemEffect;
+        [SerializeField] private float _lifeTime = 10f;
         
         public Collider ItemPrefab => _itemPrefab;
         public ItemEffect ItemEffect => _itemEffect;
+        public float LifeTime => _lifeTime;
     }
 }
--- Assets/Code/Items/ItemPickup.cs
+++ Assets/Code/Items/ItemPickup.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using Code.GeneralLogic;
 using Code.PlayerLogic;
 using UnityEngine;
@@ -9,10 +11,21 @@ namespace Code.Items
     {
         [SerializeField] private ItemConfig _itemConfig;
 
+        private void Start()
+        {
+            StartCoroutine(StartLifeTimer());
+        }
 
         public void Pickup(Player player)
         {
             _itemConfig.ItemEffect.Apply(player);
+
+            Destroy(gameObject);
+        }
+
+        private IEnumerator StartLifeTimer()
+        {
+            yield return new WaitForSeconds(_itemConfig.LifeTime);
             
             Destroy(gameObject);
         }
